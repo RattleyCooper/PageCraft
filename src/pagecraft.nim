0a3ad530fea026b9dd328404f8541b5125d70279
py-am-i <6770988+RattleyCooper@users.noreply.github.com>
2024-03-03 11:16:20 -0800

    Removed some code

--- src/pagecraft.nim
+++ src/pagecraft.nim
@@ -14,7 +14,7 @@ proc htmlInner(x: NimNode, indent = 0, stringProc = false): NimNode {.compiletim
   x.expectKind nnkStmtList
   let spaces = repeat(' ', indent)
   for y in x:
-    if stringProc:
+    if stringProc:  # Handle evaluating strings in curly braces
       result.add quote do:
         result.add `y`
       continue
@@ -29,28 +29,30 @@ proc htmlInner(x: NimNode, indent = 0, stringProc = false): NimNode {.compiletim
       var toCheck = y[0]
       var branches = y[1..^1]
       var newIfs: seq[(NimNode, NimNode)]
-      for branch in branches:
-        for j in 0 ..< branch.len - 1:
+      for branch in branches: # Every of
+        for j in 0 ..< branch.len - 1: # Handle all cases from one of
           let ncond = condition(elifBranch(newCall("==", toCheck, branch[j]), htmlInner(branch[^1], indent + 2)))
           let newBranch = (ncond, htmlInner(branch[^1], indent + 2))
           newIfs.add newBranch        
       result.add newIfStmt(newIfs) 
-      if branches[^1].kind == nnkElse:
+      if branches[^1].kind == nnkElse: # Append the else branch
         result.add htmlInner(branches[^1][0], indent + 2)
     of nnkIfStmt: # Traverse If statements.
       var newIfs: seq[(NimNode, NimNode)]
-      for k in y:
-        for j in 0 ..< k.len - 1:
+      for k in y: # Every if/elif/else
+        for j in 0 ..< k.len - 1:  # handle all and/or as well
           let ncond = k[j]
           let nbranch = (ncond, htmlInner(k[^1], indent + 2))
           newIfs.add nbranch
       result.add newIfStmt(newIfs)
-      if y[^1].kind == nnkElse:
+      if y[^1].kind == nnkElse: # Append the else branch
         result.add htmlInner(y[^1][0], indent + 2)
-    of nnkCall, nnkCommand:
+    # This is where we handle creating HTML tags
+    of nnkCall, nnkCommand: 
       var tag = y[0]
+      tag.expectKind nnkIdent
       if $tag == "divv": tag = ident("div")
-      if y.len > 2:
+      if y.len > 1:
         writeLit spaces, "<", tag, " "
         for i, n in y:
           if n.kind == nnkExprEqExpr:
@@ -64,24 +66,6 @@ proc htmlInner(x: NimNode, indent = 0, stringProc = false): NimNode {.compiletim
         if y[^1].kind == nnkStmtList:
           result.add htmlInner(y[^1], indent + 2)
         writeLit spaces, "</", tag, ">\n"
-      elif y.len == 2:
-        tag.expectKind nnkIdent
-        # Handle tags without nesting, but with params
-        if y[1].kind == nnkExprEqExpr:
-          var n = y[1]
-          writeLit spaces, "<", tag, " "
-          if n[1].kind == nnkCurly:
-            writeLit $n[0], "=\""
-            write n[1][0]
-            writeLit "\" "
-          else:
-            writeLit $y[1][0], "=", $y[1][1]
-          writeLit ">\n"
-        else:
-          writeLit spaces, "<", tag, ">\n"
-          # Recurse over child          
-          result.add htmlInner(y[1], indent + 2)
-          writeLit spaces, "</", tag, ">\n"
       else:
         writeLit spaces, "<", tag, " "
         for i, n in y:
